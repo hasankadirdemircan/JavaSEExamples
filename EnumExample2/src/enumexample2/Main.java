d2109b65658571ed887a9bb1fca48c58638d5bb9
HKDemircan <hkdemircan06@gmail.com>
2018-06-13 14:33:59 +0300

    enum örneği eklendi.

---
+++ EnumExample2/src/enumexample2/Main.java
@@ -0,0 +1,21 @@
+package enumexample2;
+
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		Main main  = new Main();
+		main.comparison(1);
+	}
+	
+	public void comparison(int value)
+	{
+		for (Employee emp : Employee.values()) {
+            if (value == emp.getKey()) {
+                System.out.println("Gelen degeri : "+  value + " olan kisi : " + emp.name());
+            }
+        }
+    }
+}
+
