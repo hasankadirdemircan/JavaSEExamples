6621aa5d0a49d477c5318a027df213818e6f84c4
HKDemircan <hkdemircan06@gmail.com>
2018-08-20 19:02:13 +0300

    updated operators example

--- Operators/src/com/hkdemircan/AssignmentTest.java
+++ Operators/src/com/hkdemircan/AssignmentTest.java
@@ -8,7 +8,8 @@ public class AssignmentTest {
 		int number = 100;
 		number -=10; // number = number - 10;
 		System.out.println("number : " + number);
-		
+		number +=20; // number = number + 20;
+		System.out.println("number : " + number);
 		int x = 5;
 		x *= 6;
 		System.out.println("x : " + x);
