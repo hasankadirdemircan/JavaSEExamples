e9437e91b53408d6e3d6500bde6b3346500fdda6
HKDemircan <hkdemircan06@gmail.com>
2018-08-24 16:25:54 +0300

    added UncaughtException Example

---
+++ UncaughtException/src/com/hkdemircan/UncaughtException.java
@@ -0,0 +1,39 @@
+package com.hkdemircan;
+
+public class UncaughtException {
+
+	public static void main(String[] args) {
+		UncaughtException uncaughtException = new UncaughtException();
+		String name = null;
+		uncaughtException.method1(name);
+	}
+	
+	public void method1(String name) {
+		System.out.println("method1");
+		try {
+			method2(name);
+		}catch(NullPointerException e) {
+			// NullPointerException burada yakalandi.
+			//Buraya kadar method3, method2 çöker.
+			System.out.println("NullPointerException Catch!");
+		}
+		System.out.println("method1 work!");
+	}
+	
+	public void method2(String name) {
+		System.out.println("method2");
+		method3(name);
+		System.out.println("method2 not work!");
+	}
+	
+	public void method3(String name) {
+		name.toLowerCase();
+		System.out.println("method3 not work!");
+	}
+}
+/// Stack Goruntusu
+//  method3
+//  method2
+//  method1
+//  main
+
